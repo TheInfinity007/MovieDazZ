43188bafbc86c6087ee8be01bd2351fda707d115
RamChandra10 <ramchandra71999@gmail.com>
2019-07-28 23:16:30 +0530

    first time uploading the files

---
+++ js/jquery_myScript.js
@@ -0,0 +1,20 @@
+// var btn = $('#go_to_top');
+// $(window).scroll(function(){
+// 	if($(window).scrollTop() > 300){
+// 		btn.addClass('show');
+// 	}
+// 	else{
+// 		btn.removeClass('show')
+// 	}
+// });
+
+// // btn.on('click', function(e){
+// // 		e.preventDefault();
+// // 		$('html, body').animate({scrollTop:0}, '300');
+// $(document).ready(function(){
+// 	btn.on('click', function(e){
+// 		e.preventDefault();
+// 		$('html, body').animate({scrollTop:0}, '300');
+// 	});	
+// });
+
